f85564675dfc0d50d77de7153d5ba9b46c6edae9
simon <arg>
2023-08-04 12:55:48 +0200

    errors

--- src/routes/create-checkout-session/+server.ts
+++ src/routes/create-checkout-session/+server.ts
@@ -1,27 +1,27 @@
 import { STRIPE_TEST_KEY, DOMAIN } from '$env/static/private';
 
-import { redirect } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 import Stripe from 'stripe'
 
-const stripe = new Stripe(STRIPE_TEST_KEY, {
-    apiVersion: '2022-11-15'
-});
 
 
 export async function POST() {
-  const session = await stripe.checkout.sessions.create({
-    line_items: [
-      {
-        price: 'price_1NaloJKIDbJkcynJshhB7EsM',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    success_url: `${DOMAIN}/success`,
-    cancel_url: `${DOMAIN}/cancel`,
-  });
-  if (session.url) {
-    throw redirect(303, session.url);
-  }
-
+    const stripe = new Stripe(STRIPE_TEST_KEY, {
+        apiVersion: '2022-11-15'
+    });
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price: 'price_1NaloJKIDbJkcynJshhB7EsM',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${DOMAIN}/success`,
+      cancel_url: `${DOMAIN}/cancel`,
+    });
+    if (session.url) {
+      throw redirect(303, session.url);
+    }
+    throw error(420, 'Enhance your calm');
 }
