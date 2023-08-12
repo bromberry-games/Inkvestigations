093909ecf8bec5a2c3e3b012c9961002b0d2490b
Simon <simon>
2023-08-12 14:10:16 +0200

    added pgsql insert trigger

--- supabase/migrations/20230809161026_create_mysteries_table.sql
+++ supabase/migrations/20230809161026_create_mysteries_table.sql
@@ -1,13 +1,17 @@
+-- Create tables
+
 CREATE TABLE mysteries (
     Name TEXT PRIMARY KEY UNIQUE,
     Description TEXT Not Null,
-    Prompt TEXT Not Null,
+    Prompt TEXT Not Null
 );
 
 CREATE TABLE user_messages (
     user_id uuid PRIMARY KEY REFERENCES auth.users(id) ON UPDATE CASCADE,
     amount INTEGER CHECK (amount >= 0)
 );
+
+-- policies
 
 alter table mysteries 
   enable row level security;
@@ -23,3 +27,19 @@ create policy "Mysteries are viewable by everyone"
 create policy "Individuals can view their own muessages."
     on user_messages for select
     using ( auth.uid() = user_id );
+
+-- triggers
+
+CREATE OR REPLACE FUNCTION add_user_to_user_messages()
+RETURNS TRIGGER AS $$
+BEGIN
+    INSERT INTO user_messages (user_id, amount) VALUES (NEW.id, 5);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE OR REPLACE TRIGGER tr_insert_user_messages
+AFTER INSERT ON auth.users
+FOR EACH ROW
+EXECUTE FUNCTION add_user_to_user_messages();
