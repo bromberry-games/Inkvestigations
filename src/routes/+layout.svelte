e223a13e3fea4582feb63d3b6bc52b29cae012e0
Zivky <zivanovicivan@pm.me>
2023-09-12 12:02:01 +0200

    added favicon and resized logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -24,7 +24,7 @@
 
 <Navbar let:hidden let:toggle class="w-full !bg-custom-secondary"  navDivClass="mx-auto flex flex-wrap justify-between items-center">
   <NavBrand href="/">
-    <img src="/images/pen_minimal.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo"/>
+    <img src="/images/logo_2.svg" class="mr-3 h-6 sm:h-12" alt="Flowbite Logo"/>
   </NavBrand>
   <div class="flex md:order-2">
     {#if session}
