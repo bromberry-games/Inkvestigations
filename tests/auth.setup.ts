8d095d9747e03ed17ebce60005c2ec961d834a0a
Simon <simon>
2023-10-13 12:35:16 +0200

    ahhhhh!

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -5,8 +5,8 @@ const authFile = 'playwright/.auth/user.json';
 
 setup('authenticate', async ({ page }) => {
   await page.setExtraHTTPHeaders({
-    "CF-Access-Client-Id": process.env.CF_Access_Client_Id,
-    "CF-Access-Client-Secret": process.env.CF_Access_Client_Secret
+    "CF-Access-Client-Id": process.env.CF_ACCESS_CLIENT_ID,
+    "CF-Access-Client-Secret": process.env.CF_ACCESS_CLIENT_SECRET
   });
 
   await page.goto('/');
