283c6d3bfa5585b5517696db7fbc69b35a405a20
simon <arg>
2023-10-28 21:09:44 +0200

    added hero page

--- src/lib/MyLayout.svelte
+++ src/lib/MyLayout.svelte
@@ -7,31 +7,34 @@
 	export let session: Session | null;
 </script>
 
-<Navbar let:hidden let:toggle class="sticky top-0 w-full !bg-transparent" navDivClass="mx-auto flex flex-wrap justify-between items-center">
+<Navbar
+	let:hidden
+	let:toggle
+	class="sticky top-0 w-full !bg-transparent"
+	id="nav"
+	navDivClass="mx-auto flex flex-wrap justify-between items-center"
+>
 	<NavBrand href="/" class="w-1/2">
 		<div class="inline font-tertiary md:w-1/2" id="logo-container">
 			{@html logo}
 		</div>
 	</NavBrand>
-	<div class="flex md:order-2">
-		{#if session}
-			<Button
-				class="transform !rounded-full bg-quaternary !px-5 !py-3 font-primary text-xl !text-tertiary transition-transform hover:scale-110"
-				on:click={() => supabase.auth.signOut()}>LOGOUT</Button
-			>
-		{:else}
-			<!-- <Button
+	{#if session}
+		<Button
+			class="transform !rounded-full bg-quaternary !px-5 !py-3 font-primary text-xl !text-tertiary transition-transform hover:scale-110"
+			on:click={() => supabase.auth.signOut()}>LOGOUT</Button
+		>
+		<!-- <Button
 				class="transform !rounded-full bg-quaternary !px-5 !py-3 font-primary text-xl !text-tertiary transition-transform hover:scale-110"
 				href="/login">LOGIN</Button
 			> -->
-		{/if}
-		<NavHamburger on:click={toggle} />
-	</div>
-	<NavUl {hidden} class="font-primary" activeClass="!text-tertiary" on:click={toggle}>
-		<NavLi href="/mysteries" class="text-4xl !text-quaternary md:mx-8">MYSTERIES</NavLi>
-		<NavLi href="/pricing" class="text-4xl !text-quaternary md:mx-8">PRICING</NavLi>
+	{/if}
+	<NavHamburger on:click={toggle} />
+	<NavUl {hidden} class="justify-end text-end font-primary" activeClass="!text-tertiary" on:click={toggle}>
+		<NavLi href="/mysteries" class="ml-8 text-4xl !text-quaternary">MYSTERIES</NavLi>
+		<NavLi href="/pricing" class="ml-8 text-4xl !text-quaternary">PRICING</NavLi>
 		{#if !session}
-			<NavLi href="/login" class="text-4xl !text-quaternary md:mx-8">LOGIN</NavLi>
+			<NavLi href="/login" class="ml-8 text-4xl !text-quaternary">LOGIN</NavLi>
 		{/if}
 	</NavUl>
 </Navbar>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,23 +31,37 @@
 </section>
 -->
 
-<section class="relative h-screen justify-center bg-[url(/images/hero_page.webp)] bg-cover bg-top bg-no-repeat">
-	<MyLayout {supabase} {session}></MyLayout>
-	<div class="flex">
-		<div class="container ml-52 mr-auto mt-52 px-4 text-center sm:px-6 lg:px-8">
-			<div class=" font-secondary">
-				<!-- Adjusting margin to push content to the left -->
-				<h1 class="mb-4 text-end text-7xl font-extrabold tracking-wide text-tertiary">INTERACTIVE MYSTERY GAMES</h1>
-				<div class="flex justify-end">
-					<p class="mb-8 mt-4 max-w-2xl text-tertiary sm:text-xl">
-						A new type of game's afoot in these AI powered mystery murder stories. Inkvestigation combines authored mysteries with dynamic
-						storytelling.
-					</p>
-				</div>
-				<div class="mt-16 flex justify-end">
-					<Button class="w-1/4 rounded-none bg-tertiary py-4 font-primary !text-4xl !text-quaternary" href="/login">TRY FOR FREE</Button>
+<div class="flex h-full flex-1">
+	<div class="h-full flex-auto bg-tertiary md:w-11/12">
+		<section class="relative h-screen justify-center bg-[url(/images/hero_page.webp)] bg-cover bg-bottom bg-no-repeat">
+			<div id="nav">
+				<MyLayout {supabase} {session}></MyLayout>
+			</div>
+			<div class="mr-44 flex justify-end">
+				<div class="container px-4 text-center sm:px-6 lg:px-8 xl:mt-52 2xl:mt-24">
+					<div class="font-secondary">
+						<!-- Adjusting margin to push content to the left -->
+						<h1 class="mb-4 text-end text-7xl font-extrabold tracking-wide text-tertiary">INTERACTIVE MYSTERY GAMES</h1>
+						<div class="flex justify-end">
+							<p class="mb-8 mt-4 max-w-2xl text-end text-tertiary sm:text-xl">
+								A new type of game's afoot in these AI powered mystery murder stories. Inkvestigation combines authored mysteries with
+								dynamic storytelling.
+							</p>
+						</div>
+						<div class="mt-16 flex justify-end">
+							<Button class="w-1/4 rounded-none bg-tertiary py-4 font-primary !text-4xl !text-quaternary" href="/login">TRY FOR FREE</Button
+							>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
+		</section>
 	</div>
-</section>
+</div>
+
+<style>
+	#nav {
+		background: linear-gradient(to bottom, rgba(255, 255, 255, 0.3), rgba(255, 255, 255, 0));
+		margin-bottom: 24px;
+	}
+</style>
