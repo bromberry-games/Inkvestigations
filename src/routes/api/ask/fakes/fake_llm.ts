a6880fed796402d62d782f3a6ab8caeec3faa3f9
simon <arg>
2024-02-06 10:35:21 +0100

    new fake

--- src/routes/api/ask/fakes/fake_llm.ts
+++ src/routes/api/ask/fakes/fake_llm.ts
@@ -20,22 +20,23 @@ export function createFakeLetterLLM(onResponseGenerated: (input: string) => Prom
 
 export function createFakeBrainLLM(): FakeListLLM {
 	return new FakeListLLM({
-		responses: [
-			`We're playing a game where I'm police chief Wellington acting on orders of Sherlock Holmes to solve a mystery. 
-				The player has asked me to search the house.
-				Have I been provided this information? 
-				Yes
-				Information:
-				- The study yielded various clues, including a bottle of medication for hair-regrowth, discarded drafts, a half-written piece on uncovering mafia dealings, letters showing Tin's innocence, Terry's writing desk with his favorite ink pen, a drawer of fan letters, and the fingerprints of all people close to him.
-				- mood: curious`,
-			`We're playing a game where I'm police chief Wellington acting on orders of Sherlock Holmes to solve a mystery. 
-				The player has greeted me. 
-				Do I have information for that? 
-				No.
-				I make up something funny but realistic. Wellington would react like this: 
-				Information:
-				- By the way, someone has reported a missing cat in the neighborhood, and they suspect it may have found its way into the local bakery.
-				- mood: bemused`
-		]
+		responses: ['Hello']
+		// responses: [
+		// `We're playing a game where I'm police chief Wellington acting on orders of Sherlock Holmes to solve a mystery.
+		// The player has asked me to search the house.
+		// Have I been provided this information?
+		// Yes
+		// Information:
+		// - The study yielded various clues, including a bottle of medication for hair-regrowth, discarded drafts, a half-written piece on uncovering mafia dealings, letters showing Tin's innocence, Terry's writing desk with his favorite ink pen, a drawer of fan letters, and the fingerprints of all people close to him.
+		// - mood: curious`,
+		// `We're playing a game where I'm police chief Wellington acting on orders of Sherlock Holmes to solve a mystery.
+		// The player has greeted me.
+		// Do I have information for that?
+		// No.
+		// I make up something funny but realistic. Wellington would react like this:
+		// Information:
+		// - By the way, someone has reported a missing cat in the neighborhood, and they suspect it may have found its way into the local bakery.
+		// - mood: bemused`
+		// ]
 	});
 }
