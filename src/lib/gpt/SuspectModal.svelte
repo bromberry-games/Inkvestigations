18f8a379d9e0898c0889065d1a28cfe8de454448
Simon <simon>
2024-02-19 11:56:45 +0100

    ring changes

--- src/lib/gpt/SuspectModal.svelte
+++ src/lib/gpt/SuspectModal.svelte
@@ -20,19 +20,30 @@
 	color="!bg-secondary"
 >
 	<div class="flex w-full flex-col">
-		<textarea  placeholder="Enter notes..." use:textareaAutosizeAction bind:value={notes.general} class="mb-2 rounded bg-amber-50 border-primary focus:ring-tertiary focus:border-primary"></textarea>
+		<textarea
+			placeholder="Enter notes..."
+			use:textareaAutosizeAction
+			bind:value={notes.general}
+			class="mb-2 rounded border-primary bg-amber-50 focus:border-tertiary focus:ring-2 focus:ring-tertiary"
+		></textarea>
 		{#each suspects as suspect}
 			<hr class="rmy-1 h-0.5 border-0 bg-gray-800 dark:bg-gray-800" />
 			<div class="mt-2 flex">
 				<div class="mr-4 flex flex-col items-center justify-between">
 					<img
-						src={'/images/mysteries/' + slug.toLowerCase() + '/suspects/' + convertToSnakeCaseEnhanced(suspect.name) + '.webp'} class="rounded-sm mb-2"
+						src={'/images/mysteries/' + slug.toLowerCase() + '/suspects/' + convertToSnakeCaseEnhanced(suspect.name) + '.webp'}
+						class="mb-2 rounded-sm"
 						alt={suspect.name}
 					/>
 				</div>
 				<div class="flex h-full w-full flex-col">
-					<p class="uppercase sm:text-md font-primary md:text-xl text-gray-800">{suspect.name}</p>
-					<textarea class="rounded h-full w-full bg-amber-50 border-primary focus:ring-tertiary focus:border-primary" placeholder="Enter notes..." use:textareaAutosizeAction bind:value={notes[suspect.name]} rows="2" 
+					<p class="sm:text-md font-primary uppercase text-gray-800 md:text-xl">{suspect.name}</p>
+					<textarea
+						class="h-full w-full rounded border-primary bg-amber-50 focus:border-tertiary focus:ring-2 focus:ring-tertiary"
+						placeholder="Enter notes..."
+						use:textareaAutosizeAction
+						bind:value={notes[suspect.name]}
+						rows="2"
 					></textarea>
 				</div>
 			</div>
