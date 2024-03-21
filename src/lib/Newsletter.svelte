ded2df1432c42d3e75dae80a4b2b28a598e724ff
zivkypc <zivanovicivan@pm.me>
2024-03-21 16:26:55 +0100

    trying to get newsletter signup to work

---
+++ src/lib/Newsletter.svelte
@@ -0,0 +1,24 @@
+<script>
+    let subscribed = false;
+    let error = '';
+    let message = '';
+  </script>
+  
+
+<div>  
+  <form method="post">
+    <label for="email">
+      Email
+      <input type="email" name="email" id="email" required>
+    </label>
+    <button type="submit">
+      Submit
+    </button>
+    {#if subscribed}
+      <p>Elementary!</p>
+    {/if}
+    {#if message}
+      <p>Whoops ! {message}</p>
+    {/if}
+  </form>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import MyLayout from '$lib/MyLayout.svelte';
+	import Newsletter from '../lib/Newsletter.svelte';
 	import { Button } from 'flowbite-svelte';
 
 	export let data;
@@ -29,12 +30,35 @@
 							<Button class="bg-tertiary-400 w-1/2 rounded-none py-4 font-primary !text-4xl !text-quaternary lg:w-1/4" href="/try-for-free"
 								>TRY FOR FREE</Button
 							>
-						</div>
-					</div>
-				</div>
-			</div>
+						</div>			
 		</section>
-	</div>
+		<div class="m-16 bg-tertiary-400 font-primary">
+  			<h2 class="text-tertiary font-extrabold text-4xl">
+    			Think Deductive, Be Detective
+  			</h2>
+  			<p class="mt-4 mr-8 font-secondary font-bold">
+    			In Inkvestigations game you slip into the shoes of the greatest detective: Sherlock Holmes. 
+    			A crime has occurred and police chief Wellington needs your help to solve it. By writing orders to the chief, he will perform those tasks to the best of his ability in order to give you more clues. When you have gathered enough clues, you will tell Wellington your solution. If you are right the criminal will be caught!
+  			</p>
+  			<h2 class="text-tertiary mt-8 font-extrabold text-4xl text-right">
+    			Test your smarts
+  			</h2>
+  			<p class="mt-4 font-secondary font-bold text-right">
+    			This is a game of wits. Facing off against the worst of criminals is no easy task. They make it harder by obscuring their deeds with many misleads, and sometimes Wellington is not a great help. In times like these, you will have to persevere even if the next step is not so clear. And who knows, sometimes you'll get lucky and a new event will reveal the missing piece.
+  			</p>
+  			<h2 class="text-tertiary font-extrabold mt-8 text-4xl">
+    			Countless possibilities
+  			</h2>
+  			<p class="mt-4 font-secondary font-bold">
+    			No more invisible walls. Thanks to the power of AI, you can pursue any lead that strikes you as important. But beware: not every lead ends with something useful. You must see through the noise and separate the important clues from unimportant information.
+  			</p>
+  			<h2 class="text-tertiary font-extrabold mt-8 text-4xl text-right">
+    			Challenge your friends
+  			</h2>
+  			<p class="mt-4 font-secondary font-bold text-right">
+    			You can create your own mysteries--fast! Want to challenge a friend to a battle of wits? Go to the mystery creator and set up your story. Easily write and publish your case and send a link to your friends.
+  			</p>
+		</div>
 </div>
 
 <style>
